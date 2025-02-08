8d9ff332a9b5613e81d86598918354ad8b04c71e
AnjalyPrakash <anjalyprakaash@gmail.com>
2025-02-08 11:30:21 +0530

    'update'

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
         <div>
             <div className='bg-dark text-light fixed-bottom'>
                 <div className='container d-flex'>
-                    <p className='fw-bold pt-3'>Copyright © 2024 <span className='text-warning'>ANJALY PRAKASH</span> | Portfolio. All Rights Reserved</p>
+                    <p className='fw-bold pt-3'>Copyright © 2024 <Link to={'/'} className='text-decoration-none'><span className='text-warning'>ANJALY PRAKASH</span></Link> | Portfolio. All Rights Reserved</p>
                     <div className='ms-auto d-flex  p-2'>
                         <div className='square-background me-2'>
                             <a href='https://www.instagram.com/anju__z' className='text-decoration-none text-light'><i class="fa-brands fa-instagram fa-1x shadow"></i></a>
